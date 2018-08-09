415ddad3a32d51c940fdbaeae1d152630f9cc2de
suxiaobo <suxiaobo@xgd.com>
2018-08-08 23:08:38 -0700

    ﻿修改人：苏小波
    目前功能描述：记录输入字符串并保存到指定文件，ESC退出编辑
    
    提交原因：增加日志文件note.log
    
    输出文件：test
    
    缺陷：无法识别方向键，无法移动光标
    
    计划：加入curses库来识别方向键，改用fopen、fwrite、fseek
    
    时间：Wed Aug 8 23:08:29 PDT 2018
    ************************************

--- note.c
+++ note.c
@@ -1,12 +1,23 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 #include<fcntl.h>
 #include<curses.h>
-
 #define FLAG O_CREAT | O_RDWR 
 
 #define MODE S_IRWXU | S_IWOTH| S_IXOTH | S_IROTH
 
+//#define ESC "\033"
+#define UP "\033[A"
+#define DOWN "\033[B"
+#define RIGHT "\033[C"
+#define LEFT "\033[D"
+
+#define ESC 27
+/*#define UP 72
+#define DOWN 80
+#define RIGHT 75
+#define LEFT 77*/
 int createFile()
 {
     int fd;
@@ -23,10 +34,6 @@ int createFile()
     return fd;
 }
 
-/**
-函数名称：
-
-**/
 size_t writeFile(int fd)
 {
     size_t wt;
@@ -35,10 +42,31 @@ size_t writeFile(int fd)
     for(i=0;i<4096;i++)
     {
         str[i]=getchar();
-        if(27==str[i])
+        if(ESC==str[i])
         {
             break;
         }
+		
+		if(!strcmp(str[i], UP))
+		{
+			printf("这是上键");
+			i--;
+		}
+		/*if(UP==str[i])
+		{
+			printf("\033[?25h");
+			i--;
+		}*/
+		/*if('s'==str[i])
+		{
+			printf("\033[1B");			
+			str[i]='\n';
+		}
+		if('a'==str[i])
+		{
+			printf("\033[1D");
+			
+		}*/
     }
    // printf("%s\n",str);
     wt=write(fd,str,strlen(str));
